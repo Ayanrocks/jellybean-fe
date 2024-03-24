4055f4be88cbd4689f9c71b08e4bb68fac70553d
Ayan Banerjee <ayanrocks@Ayans-MacBook-Pro-2.local>
2024-03-24 14:17:51 +0530

    Initial Commit With Landing Page

---
+++ src/app/background.jsx
@@ -0,0 +1,38 @@
+//Background.js
+import { useState, useRef, useEffect } from 'react';
+import FOG from 'vanta/dist/vanta.fog.min';
+import * as THREE from 'three';
+
+export default function Background({ width, height, children }) {
+  const [vantaEffect, setVantaEffect] = useState(0);
+
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        FOG({
+          THREE,
+          el: vantaRef.current,
+          mouseControls: 'true',
+          touchControls: 'true',
+          gyroControls: 'false',
+          minHeight: height,
+          minWidth: width,
+          highlightColor: 0x003566,
+          midtoneColor: 0x001d3d,
+          lowlightColor: 0x003566,
+          baseColor: 0x000814,
+          blurFactor: 0.65,
+          speed: 3.5,
+          zoom: 1.8,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return <div ref={vantaRef}>{children}</div>;
+}
